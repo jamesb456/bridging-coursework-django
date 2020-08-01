0cd7920803db045838986cc62c95ce79f09882b6
jamesb456 <james.bray467@gmail.com>
2020-08-01 16:33:29 +0100

    CSS and layout changes

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
-
+from django.template.defaultfilters import slugify
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    slug = models.SlugField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
@@ -16,3 +17,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = self.slug or slugify(self.title)
+        super().save(*args, **kwargs)
