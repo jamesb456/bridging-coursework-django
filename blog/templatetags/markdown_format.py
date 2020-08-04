867ef155c7aa07dfee3dd4377e4b86d65b95f320
James Bray <james.bray467@gmail.com>
2020-08-04 13:55:08 +0100

    Nicer looking w/ text centering, markdown support

---
+++ blog/templatetags/markdown_format.py
@@ -0,0 +1,21 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+import markdown as md
+
+register = template.Library()
+
+
+@register.filter()
+@stringfilter
+def markdown(value):
+    return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
+
+@register.filter()
+@stringfilter
+def markdown_preview(value : str):
+    if(len(value) < 400):
+        return markdown(value)
+    else:
+        end_index = value[400:].find('\n') + 400
+        return markdown(value[0:end_index] + "\n...")
