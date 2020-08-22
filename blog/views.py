0a0a22625aef417b3069fcd8c2e1370599c59979
James Bray <james.bray467@gmail.com>
2020-08-22 14:11:48 +0100

    Added new app for cv, moved tests over as well

--- blog/views.py
+++ blog/views.py
@@ -7,13 +7,12 @@ from django.shortcuts import redirect
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    print(f"Posts={posts}")
     return render(request,'blog/post_list.html',{'posts': posts})
 
 def post_detail(request,pk,slug):
-    print(f"Primary={pk}, Slug = {slug}")
+    
     post = get_object_or_404(Post,pk=pk)
-    print(post)
+    
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
+    path('', lambda req: redirect('/blog/'))
 ]
