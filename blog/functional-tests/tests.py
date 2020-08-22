b3c501ee85d30d57892cf7701df9124b26a8668d
James Bray <james.bray467@gmail.com>
2020-08-22 12:46:27 +0100

    First bit of editing cv test

--- blog/functional-tests/tests.py
+++ blog/functional-tests/tests.py
@@ -22,7 +22,50 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.quit()
 
 
+    def test_can_edit_cv(self):
+        # James would like to edit his own CV. He goes to
+        # his personal webpage and clicks on the CV button
+        button = self.browser.find_element_by_id('id_btn_cv')
+        button.click()
+        # The page's title is 'CV'
+        self.assertEqual('CV',self.browser.title)
 
+        # In the header he sees a button to edit his CV. He clicks on it
+        button = self.browser.find_element_by_id('id_btn_edit_cv')
+        # He is taken to a page titled 'Edit CV'
+        self.assertEqual('Edit CV',self.browser.title)
+
+        self.fail("Finish the test")
+        # Firstly, James needs to edit his e-mail address. He finds a text box labeled
+        # 'e-mail' and replaces the text with 'jxb1123@student.bham.ac.uk'
+
+        # James then wants to edit his personal statement by adding a sentence.
+        # He finds a text box labeled 'Personal Statement' and adds a sentence
+        # about his passion for Test Driven Development
+
+        # Next, James looks at a section of the page entitled 'Education'.
+        # He realises he needs to add an extra qualification to reflect his
+        # mastery of TDD. Therefore:
+        # He first presses the button 'Add qualification'
+
+        # This reveals four new text boxes and a 'Submit' button
+
+        # In the text box labeled 'Title' James writes 'MSci Testing, Driving and Developing'
+
+        # In the text box labeled 'Start Date' James writes '16/05/2020'
+
+        # In the text box labeled 'End Date' James writes '22/08/2020'
+        
+        # In the text box labeled 'Description' James writes 'something about goats'
+
+        # He then presses the 'Submit' button. 
+        # There is now a table showing the details he just entered
+
+        
+
+
+        
+        
     
     def test_user_facing(self):
         # Edith want's to view someone's CV. She goes to their
@@ -31,11 +74,12 @@ class NewVisitorTest(LiveServerTestCase):
 
         # The page has a link to a CV in the header,
         # which she clicks on.
-
+        self.fail("Finish this test!")
         # She finds that she is immediately presented with
         # their name, picture and contact details i.e
         # * Github profile
         # * LinkedIn Profile
+        # * e-mail
 
         # Further down the page she sees a section of the page titled 
         # 'Personal Profile'
